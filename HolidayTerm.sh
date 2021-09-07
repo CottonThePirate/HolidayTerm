75b5824a779e0db06bae54d824208a3b0b4c4ccb
servy <servy>
2021-09-06 14:09:26 -1000

    Added calendar command parsing. Next up to add calendar as option and to put the calendar into an associative array

--- HolidayTerm.sh
+++ HolidayTerm.sh
@@ -2,6 +2,25 @@
 # Chris Schaab 2021
 # This script looks a list of holidays, and prints the number of days to
 # the next one in the list. 
+#
+#
+# #Calendar File 
+
+debug=1
+
+CalendarFile="/usr/share/calendar/calendar.usholiday"
+
+#Store the next 360 days of holidays into a varb
+#I was going to make it less to conserve CPU power, 
+#Then realized my $30 rasperry pi that I'm developing on could process them in 
+#like 4 ms. Sloppy, but what if someone only wants to celebrate one thing a year? 
+	
+declare -A Holidays
+
+Holidays="$(calendar -A 360 -f $CalendarFile)" 
+
+#echo $Holidays
+
 
 
 #holiday Dictionary aka associatve array
