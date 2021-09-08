9a11ea54b26d20b6b0b88b5e72a41be3acecaf74
servy <servy>
2021-09-08 12:21:18 -1000

    Added in calendar parsing

--- HolidayTerm.sh
+++ HolidayTerm.sh
@@ -8,19 +8,45 @@
 
 debug=1
 
+#File to be used for holiday calculation, must be in the format for the calendar command
 CalendarFile="/usr/share/calendar/calendar.usholiday"
+
+
+# Store todays date in seconds (unix time) in today
+today=$(date '+%s')
+
+
+
 
 #Store the next 360 days of holidays into a varb
 #I was going to make it less to conserve CPU power, 
 #Then realized my $30 rasperry pi that I'm developing on could process them in 
 #like 4 ms. Sloppy, but what if someone only wants to celebrate one thing a year? 
 	
-declare -A Holidays
+#Assign Holidays the next 360 days worth of holidays from CalendarFile, 
+#us tr -d to trim out the * used to designate a holiday without a fixed date (ie 3rd tuesday of march) 
 
-Holidays="$(calendar -A 360 -f $CalendarFile)" 
+Holidays="$(calendar -A 360 -f $CalendarFile | tr -d "*" )" 
 
-#echo $Holidays
+#debug echo holidays
 
+echo $Holidays
+
+#go thru our holiday list one by one until we find one with a matching ascii art file
+
+while IFS= read -r line; do
+    	#echo "Text read from file: $line"
+    DateNum="$(cut -d$'\t' -f 1 <(echo "$line"))"
+    DateText="$(cut -d$'\t' -f 2 <(echo "$line"))"
+    
+    	#How Many Days until the next Holiday?
+    date     
+
+    echo "$DateText"
+done < <(printf '%s\n' "$Holidays")
+
+
+#Display the file 
 
 
 #holiday Dictionary aka associatve array
@@ -37,7 +63,6 @@ Holidays="$(calendar -A 360 -f $CalendarFile)"
 July4th=$(date --date=$July4th '+%s')
 
 
-today=$(date '+%s')
 
 
 
