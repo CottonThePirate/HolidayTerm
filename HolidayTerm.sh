2708c2a97c43c2b5e60230681e9d388d7e21142b
servy <servy>
2021-09-08 15:02:15 -1000

    Added in days til parsing

--- HolidayTerm.sh
+++ HolidayTerm.sh
@@ -23,14 +23,14 @@ today=$(date '+%s')
 #Then realized my $30 rasperry pi that I'm developing on could process them in 
 #like 4 ms. Sloppy, but what if someone only wants to celebrate one thing a year? 
 	
-#Assign Holidays the next 360 days worth of holidays from CalendarFile, 
+#Assign Holidays the next 60 days worth of holidays from CalendarFile, 
 #us tr -d to trim out the * used to designate a holiday without a fixed date (ie 3rd tuesday of march) 
 
-Holidays="$(calendar -A 360 -f $CalendarFile | tr -d "*" )" 
+Holidays="$(calendar -A 60 -f $CalendarFile | tr -d "*" )" 
 
 #debug echo holidays
 
-echo $Holidays
+#echo $Holidays
 
 #go thru our holiday list one by one until we find one with a matching ascii art file
 
@@ -38,36 +38,27 @@ while IFS= read -r line; do
     	#echo "Text read from file: $line"
     DateNum="$(cut -d$'\t' -f 1 <(echo "$line"))"
     DateText="$(cut -d$'\t' -f 2 <(echo "$line"))"
+    DateSec=$(date --date=$DateNum '+%s')
     
     	#How Many Days until the next Holiday?
-    date     
+	daysToHoliday=$(((DateSec - today)/86400))
+#	if daysToHoliday==0 then
+#		echo "Today is $DateText"
 
-    echo "$DateText"
+
+    echo "$DateText is $daysToHoliday days away"
 done < <(printf '%s\n' "$Holidays")
 
 
-#Display the file 
 
-
-#holiday Dictionary aka associatve array
-#declare -A holidays
-#holidays[NewYearsDay]="Jan 1"
-#holidays[ValentinesDay]="Feb 14"
-#holidays[July4th]="Jul 4"
-#holidays[Halloween]="Oct 31"
-#holidays[Christmas]="Dec 25"
-#holidays[NewYearsEve]="Dec 31"
-
-
-
-July4th=$(date --date=$July4th '+%s')
+#July4th=$(date --date=$July4th '+%s')
 
 
 
 
 
 
-for key val in "${(@kv)holidays}"; do
-    echo "$key -> $val"
-    Days2Halloween=$(((NewYears - today) / 86400))
-done
+#for key val in "${(@kv)holidays}"; do
+#    echo "$key -> $val"
+#    Days2Halloween=$(((NewYears - today) / 86400))
+#done
