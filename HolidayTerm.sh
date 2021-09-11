93ffccbc47270de25f0898c750aa37d75946503d
servy <servy>
2021-09-11 05:53:14 -1000

    Added art directory for ascii art to keep main dir clean

--- HolidayTerm.sh
+++ HolidayTerm.sh
@@ -10,23 +10,21 @@ debug=1
 
 #File to be used for holiday calculation, must be in the format for the calendar command
 CalendarFile="/usr/share/calendar/calendar.usholiday"
+ArtFilesDir="./holidayArt"
 
 
 # Store todays date in seconds (unix time) in today
 today=$(date '+%s')
 
 
+#Assign Holidays the next 60 days worth of holidays from CalendarFile, more days takes longer due to the
+#many calls to the "date" program to do date math, this is fine on a real computer, but a raspberry pi it will add like 750ms to login
 
+daysToCheck=60
 
-#Store the next 360 days of holidays into a varb
-#I was going to make it less to conserve CPU power, 
-#Then realized my $30 rasperry pi that I'm developing on could process them in 
-#like 4 ms. Sloppy, but what if someone only wants to celebrate one thing a year? 
-	
-#Assign Holidays the next 60 days worth of holidays from CalendarFile, 
-#us tr -d to trim out the * used to designate a holiday without a fixed date (ie 3rd tuesday of march) 
+#use tr -d to trim out the * used to designate a holiday without a fixed date (ie 3rd tuesday of march) 
 
-Holidays="$(calendar -A 60 -f $CalendarFile | tr -d "*" )" 
+Holidays="$(calendar -A $daysToCheck -f $CalendarFile | tr -d "*" )" 
 
 #debug echo holidays
 
@@ -38,14 +36,14 @@ while IFS= read -r line; do
     	#echo "Text read from file: $line"
     DateNum="$(cut -d$'\t' -f 1 <(echo "$line"))"
     DateText="$(cut -d$'\t' -f 2 <(echo "$line"))"
+    DateFirstWord="$(cut -d$' ' -f 1 <(echo "$DateText"))"
     DateSec=$(date --date=$DateNum '+%s')
+    if test -f "$ArtFilesDir/$DateFirstWord"; then
+	    cat "$ArtFilesDir/$DateFirstWord"
+    fi
     
     	#How Many Days until the next Holiday?
-	daysToHoliday=$(((DateSec - today)/86400))
-#	if daysToHoliday==0 then
-#		echo "Today is $DateText"
-
-
+    daysToHoliday=$(((DateSec - today)/86400))
     echo "$DateText is $daysToHoliday days away"
 done < <(printf '%s\n' "$Holidays")
 
