414e6bae866d1b19a10a608c7f4ba092271cdb85
servy <servy>
2021-09-13 09:26:41 -1000

    Testing zsh date math for portability

--- HolidayTerm.sh
+++ HolidayTerm.sh
@@ -8,10 +8,10 @@
 #1. Date command works differently on Linux and MacOS/BSD so this script only works on linux atm
 #2. Filename matching on first word only is a poor choice
 #2.a choice one would be to match on the whole holiday, but many have special characters ( ) * etc
-#2.b Could include a caldenar file with the program, but this makes it less portable
+#2.b Could include a calendar file with the program, but this makes it less portable
 #2.c Could use first 20 characters of holiday, replaceing any non 0-9a-z chars with _ 
 #
-
+zmodload zsh/datetime
 debug=1
 
 #File to be used for holiday calculation, must be in the format for the calendar command
@@ -19,8 +19,8 @@ CalendarFile="/usr/share/calendar/calendar.usholiday"
 ArtFilesDir="./holidayArt"
 
 
-# Store todays date in seconds (unix time) in today
-today=$(date '+%s')
+# Store todays date in seconds (unix time) in today zsh has a built in command for this! 
+today="$(print -P "%D{%s}")"
 
 
 #Assign Holidays the next 60 days worth of holidays from CalendarFile, more days takes longer due to the
@@ -37,7 +37,7 @@ Holidays="$(calendar -A $daysToCheck -f $CalendarFile | tr -d "*" )"
 #go thru our holiday list one by one until we find one with a matching ascii art file
 
 while IFS= read -r line; do
-	# The calendar command outputs text in date <tab> Name of holiday \n 
+	# The calendar command outputs text as: date <tab> Name of holiday \n 
 	# The cut command will split on the tab to seperate the date and the name of the holiday
     DateNum="$(cut -d$'\t' -f 1 <(echo "$line"))"
     DateText="$(cut -d$'\t' -f 2 <(echo "$line"))"
@@ -48,8 +48,10 @@ while IFS= read -r line; do
 	    cat "$ArtFilesDir/$DateFirstWord"
     fi
     
+    #zsh date notes strftime -r "%Y %m %d" "2022 02 11" provides sec from epoch of date
     	#How Many Days until the next Holiday?
-    DateSec=$(date --date=$DateNum '+%s')
+    #DateSec=$(date --date=$DateNum '+%s')
+    DateSec="$(strftime -r "%Y %b %d" "2021 $DateNum" )"
     daysToHoliday=$(((DateSec - today)/86400))
     echo "$DateText is $daysToHoliday days away"
 done < <(printf '%s\n' "$Holidays")
