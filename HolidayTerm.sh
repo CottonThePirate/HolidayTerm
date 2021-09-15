c55692d509fd34d56721cf8e7b11b76e287b8e34
Chris Schaab <cschaab@Compy9000.local>
2021-09-14 15:45:45 -1000

    Only display one artwork, rearranged some lines for clarity

--- HolidayTerm.sh
+++ HolidayTerm.sh
@@ -7,22 +7,36 @@
 #CURRENT ISSUES
 #
 
-#zsh Module datetime provides portable date math and other useful items
-zmodload zsh/datetime
 
 #File to be used for holiday calculation, must be in the format for the calendar command
 CalendarFile="/usr/share/calendar/calendar.usholiday"
 ArtFilesDir="./holidayArt"
 
 
-# Store todays date in seconds (unix time) zsh/datetime provides as EPOCHSECONDS
-today=$EPOCHSECONDS
-
-
 #Assign Holidays the next 60 days worth of holidays from CalendarFile, more days takes longer due to the
 #many calls to the "date" program to do date math, this is fine on a real computer, but a raspberry pi it will add like 750ms to login
 
 daysToCheck=60
+
+#Limit the display of ascii art to 1 file so that you don't get something like
+#Thanksgiving, christmas, new years, all displaying art at once
+artsToDisplay=1
+artsDisplayed=0
+
+
+###############
+#No user variables beyond this point
+###############
+
+
+
+
+#zsh Module datetime provides portable date math and other useful items
+zmodload zsh/datetime
+
+# Store todays date in seconds (unix time) zsh/datetime provides as EPOCHSECONDS
+today=$EPOCHSECONDS
+
 
 #use tr -d to trim out the * used to designate a holiday without a fixed date (ie 3rd tuesday of march) 
 
@@ -41,11 +55,13 @@ while IFS= read -r line; do
     	#Set the artfile to look for name to on alpha numeric chars (no special or spaces)
     ArtFile=${DateText//[^[:alnum:]-]/_}
     
-    	#pick the first word of the holiday for ascii art matching, this should be more robust
-    DateFirstWord="$(cut -d$' ' -f 1 <(echo "$DateText"))"
     	#If there is a file with the same name as the holiday, cat the file
-    if test -f "$ArtFilesDir/$ArtFile"; then
-	    cat "$ArtFilesDir/$ArtFile"
+    if test -f "$ArtFilesDir/$ArtFile" ; then 
+	    #only cat the number of files in artsToDisplay
+	    if test "$artsDisplayed" -lt "$artsToDisplay"; then
+	    	cat "$ArtFilesDir/$ArtFile"
+	    	((artsDisplayed++))
+	    fi
     fi
     
     	#How Many Days until the next Holiday?
