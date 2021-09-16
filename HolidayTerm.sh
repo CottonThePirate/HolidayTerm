483fb9bd17af887421a93f0197724564608052c2
Chris Schaab <cschaab@Compy9000.local>
2021-09-15 15:30:43 -1000

    added in file name display mode to assist with art file names

--- HolidayTerm.sh
+++ HolidayTerm.sh
@@ -7,21 +7,31 @@
 #CURRENT ISSUES
 #
 
+#zparseopts -A -D 
 
 #File to be used for holiday calculation, must be in the format for the calendar command
 CalendarFile="/usr/share/calendar/calendar.usholiday"
 ArtFilesDir="./holidayArt"
 
+#FileName Check this mode will go thru all 365 days (to account for leap years) and display the file name
+#for every holiday in the calendar file, and if that file appears to exist currently
 
-#Assign Holidays the next 60 days worth of holidays from CalendarFile, more days takes longer due to the
-#many calls to the "date" program to do date math, this is fine on a real computer, but a raspberry pi it will add like 750ms to login
-
-daysToCheck=60
+FileNameCheckMode=0
 
 #Limit the display of ascii art to 1 file so that you don't get something like
 #Thanksgiving, christmas, new years, all displaying art at once
 artsToDisplay=1
 artsDisplayed=0
+#
+#
+#Assign Holidays the next 60 days worth of holidays from CalendarFile, more days takes longer due to the
+#many calls to the "date" program to do date math, this is fine on a real computer, but a raspberry pi it will add like 750ms to login
+
+if test "$FileNameCheckMode" -eq 0; then
+	daysToCheck=60
+else
+        daysToCheck=365
+fi
 
 
 ###############
@@ -55,28 +65,23 @@ while IFS= read -r line; do
     	#Set the artfile to look for name to on alpha numeric chars (no special or spaces)
     ArtFile=${DateText//[^[:alnum:]-]/_}
     
-    	#If there is a file with the same name as the holiday, cat the file
-    if test -f "$ArtFilesDir/$ArtFile" ; then 
-	    #only cat the number of files in artsToDisplay
+    if test "$FileNameCheckMode" -eq 1; then
+	    echo
+	    echo "$DateText occurs on $DateNum and looks for the file"
+	    echo "$ArtFilesDir/$ArtFile"
+    	
+	#If there is a file with the same name as the holiday, cat the file
+    elif test -f "$ArtFilesDir/$ArtFile" ; then 
+	    	#only cat the number of files in artsToDisplay
 	    if test "$artsDisplayed" -lt "$artsToDisplay"; then
 	    	cat "$ArtFilesDir/$ArtFile"
 	    	((artsDisplayed++))
 	    fi
     fi
+    		#How Many Days until the next Holiday?
+		#send errors to the bit bucket because sometimes the date slightly mismatches
+    	DateSec="$(strftime -r "%Y %b %d" "2021 $DateNum" )" &>/dev/null
+    	daysToHoliday=$(((DateSec - today)/86400))
+    	echo "$DateText is $daysToHoliday days away"
     
-    	#How Many Days until the next Holiday?
-	#send errors to the bit bucket because sometimes the date slightly mismatches
-    DateSec="$(strftime -r "%Y %b %d" "2021 $DateNum" )" &>/dev/null
-    daysToHoliday=$(((DateSec - today)/86400))
-    echo "$DateText is $daysToHoliday days away"
 done < <(printf '%s\n' "$Holidays")
-
-
-
-
-#Old cruft from when I was trying to use bash "dictionaries" 
-
-#for key val in "${(@kv)holidays}"; do
-#    echo "$key -> $val"
-#    Days2Halloween=$(((NewYears - today) / 86400))
-#done
